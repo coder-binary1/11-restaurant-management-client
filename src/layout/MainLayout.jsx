a2c1db0b2b060d8a406d1231a7971ff0e4d006ad
coder-binary1 <noman.g341@gmail.com>
2025-04-06 23:48:35 +0600

    project initialized; install -> react-router, tailwind & daisyUi; setup -> router &  router provider

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import Footer from "../Pages/Shared/Footer";
+import Navbar from "../Pages/Shared/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
