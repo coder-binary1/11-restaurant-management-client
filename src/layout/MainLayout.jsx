61d0e42a111b3ece04d4b2746963fc2d29f90b57
coder-binary1 <noman.g341@gmail.com>
2025-04-10 20:59:33 +0600

    scroll to top feature added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer";
 import Navbar from "../Pages/Shared/Navbar";
 import { ToastContainer } from "react-toastify";
 import ThemeProvider from "../Context/ThemeContext/ThemeProvider";
+import { useEffect } from "react";
 
 const MainLayout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <div className="flex flex-col h-screen mx-auto font-poppins">
       <ThemeProvider>
